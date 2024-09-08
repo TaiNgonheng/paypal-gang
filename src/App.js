fa9469148abf7fff833a970db18e9b764c2078f3
TaiNgonheng <heng68807@gmail.com>
2024-09-08 15:39:14 +0700

    hi

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 style={{fontSize:24,color:"red",textAlign:"center"}}>Welcome to Paypal Gang</h1>
     </div>
   );
 }
